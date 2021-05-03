10d02a11377717ea5e85ece1d3452f88525efa0f
salhoub <aymenbelhadjkacem@gmail.com>
2021-05-03 23:13:38 +0100

    r

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-import sys
-s = 0
 if __name__ == "__main__":
-    if len(sys.argv) - 1 == 0:
-        print("0")
-    else:
-        for i in range(1, len(sys.argv)):
-            s += int(sys.argv[i])
-print(s)
+    import sys
+    nbar = len(sys.argv) - 1
+    res = 0
+    for i in range(1, nbar + 1):
+        res += int(sys.argv[i])
+print(res)
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    path = dir(hidden_4)
-    for i in range(0, len(path)):
-        if path[i][0] != '_':
-print(path[i], end="\n")
+    for i in dir(hidden_4):
+        if (i[:2] != "__"):
+print(i)
