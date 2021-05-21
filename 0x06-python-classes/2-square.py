062ab86e2cdd7605c60e991fb0885d72b434506e
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2021-05-21 16:04:01 +0100

    r

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,8 @@
 class Square:
     """Empty Square class"""
     def __init__(self,size=0):
-        if not type(self.size) is int:
+        self.__size=size
+        if not type(self.__size) is int:
             raise TypeError("only integers are allowed")
         if (self.raise < 0):
             raise ValueError("self must be positive")
