6771deea42605fa90bd66e318ada6e4ecac8a62c
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2021-05-21 15:57:41 +0100

    r

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,15 @@
+
+
 #!/usr/bin/python3
+""" Module Sqaure """
 class Square:
-    def __init__(self, size=0):
-        if type(size) != int:
-            print("size must be an integer", end="")
-            raise TypeError
-        if size >= 0:
-            self.__size = size
-        else:
-            print("size must be >= 0", end="")
-            raise ValueError
+    """Empty Square class"""
+    def __init__(self,size=0):
+        try
+            self.size = int(self.size)
+            self.size >= 0 
+        expect 	TypeError
+           print ('TypeError')
+        expect ValueError
+           print('ValueError')
+    pass
