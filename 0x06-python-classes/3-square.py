0ea309d715e95dc9eeecd23b555e9c039819bf87
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2021-05-21 18:16:18 +0100

    first tempt

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,15 +1,23 @@
-
-
 #!/usr/bin/python3
-""" Module Sqaure """
+"""Module square empty"""
+
+
 class Square:
-    """Empty Square class"""
-    def __init__(self,size=0):
-        try
-            self.size = int(self.size)
-            self.size >= 0 
-        expect 	TypeError
-           print ('TypeError')
-        expect ValueError
-           print('ValueError')
+
+    """Square class def"""
+    def __init__(self, size=0):
+        """if size not integer test raise expectation"""
+        """ attribute size (int): Size of square"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+            """second test if size is negative it must be positive"""
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+            """if size is positive init it"""
+        else:
+            self.__size = size
+    pass
+    def area(self):
+        area = self.__size*self.__size
+        print("Area: "%d,area)
     pass
