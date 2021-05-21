a95d08df21e3b14b6ab76aac6c8dbb230fd30108
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2021-05-21 16:51:34 +0100

    r

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,11 +3,11 @@
 class Square:
     """Empty Square class"""
     def __init__(self,size=0):
-        self.__size=size
         if type(size) !=  int:
             print ("must be int")
             raise TypeError
         if size < 0:
-            raise ValueError("self must be positive")
+            print("Must be positive")
+            raise ValueError
     self.__size=size
     pass
