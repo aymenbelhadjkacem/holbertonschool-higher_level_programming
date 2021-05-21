fbe91cd56bbeb5735f7d426e89ae680760897db4
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2021-05-21 17:15:51 +0100

    1-square doc

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,10 @@
-
 #!/usr/bin/python3
-""" Module Sqaure """
+"""Module square empty"""
+
+
 class Square:
+
     """Empty Square class"""
-    def __init__(self,size):
-        self.__size=size
+    def __init__(self, size):
+        self.__size = size
     pass
