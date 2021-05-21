cbdd702ddf123a9e74c075c50d1d1abfcda138e5
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2021-05-21 17:02:52 +0100

    cc

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 """ Module Sqaure """
 class Square:
-    """Empty Square class"""
+    """Testing size  Square class attribute"""
     def __init__(self,size=0):
+        #if size not integer test raise expectation
         if type(size) !=  int:
             print ("must be int")
             raise TypeError
+        #second test if size is negative it must be positive
         if size < 0:
             print("Must be positive")
             raise ValueErro
+        #if size is positive init it 
         if size >= 0:
             self.__size=size
     pass
+
