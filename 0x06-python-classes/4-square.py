218ae4dace09ca315775cd0eb2cc9d71f395152e
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2021-05-21 18:32:24 +0100

    4-square first atempt

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,22 +1,38 @@
 #!/usr/bin/python3
-class Square:
-    def __init__(self, size=0):
-        self.size = size
+"""Module square empty"""
 
-    @property
+
+class Square:
+
+    """Square class def"""
+    def __init__(self, size=0):
+        """if size not integer test raise expectation"""
+        """ attribute size (int): Size of square"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+            """second test if size is negative it must be positive"""
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+            """if size is positive init it"""
+        else:
+            self.__size = size
+    pass
+    """are function defintion"""
+    def area(self):
+        """return the area of square"""
+        return self.__size**2
+    """getter of size"""
     def size(self):
         return self.__size
-
-    @size.setter
-    def size(self, value):
-        if type(value) != int:
+    """setter of size"""
+    def size(self, size):
+        """if size not integer test raise expectation"""
+        """ attribute size (int): Size of square"""
+        if type(size) != int:
             raise TypeError("size must be an integer")
-        if value >= 0:
-            self.__size = value
+            """second test if size is negative it must be positive"""
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+            """if size is positive init it"""
         else:
-            print("size must be >= 0", end="")
-            raise ValueError
-
-    def area(self):
-        size = self.size
-        return size * size
+            self.__size = size
