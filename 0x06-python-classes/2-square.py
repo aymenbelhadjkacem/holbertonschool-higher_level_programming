da7ef2e4970460a607136759faf390aead6d3913
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2021-05-21 17:18:27 +0100

    2-square doc

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-""" Module Sqaure """
+"""Module square empty"""
+
+
 class Square:
-    """Testing size  Square class attribute"""
-    def __init__(self,size=0):
-        #if size not integer test raise expectation
-        if type(size) !=  int:
-            print ("must be int")
+
+    """Square class def"""
+    def __init__(self, size=0):
+        """if size not integer test raise expectation"""
+        if type(size) != int:
+            print("must be int")
             raise TypeError
-        #second test if size is negative it must be positive
+        """second test if size is negative it must be positive"""
         if size < 0:
             print("Must be positive")
             raise ValueErro
-        #if size is positive init it 
+        """if size is positive init it"""
         if size >= 0:
-            self.__size=size
+            self.__size = size
     pass
-
