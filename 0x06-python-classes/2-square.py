654ffea1f177221594f1eabea013727050806b7f
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2021-05-21 17:40:59 +0100

    2-square fix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,10 +9,10 @@ class Square:
         """if size not integer test raise expectation"""
         if type(size) != int:
             raise TypeError("must be int")
-        """second test if size is negative it must be positive"""
+            """second test if size is negative it must be positive"""
         elif size < 0:
             raise ValueError("Value Error must be positive")
-        """if size is positive init it"""
+            """if size is positive init it"""
         else:
             self.__size = size
     pass
