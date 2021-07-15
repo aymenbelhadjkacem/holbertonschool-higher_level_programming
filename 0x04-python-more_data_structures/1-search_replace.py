b3ac467ae1e95fe6e974a825db3fd6bd188f0e80
ubuntu <aymenbelhadjkacem@gmail.com>
2021-07-15 04:17:19 -0700

    searchreplace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,4 +3,4 @@ def search_replace(my_list, search, replace):
     new_list = []
     for i in range(len(my_list)):
         new_list.append(replace if my_list[i] == search else my_list[i])
-return (new_list)
+    return (new_list)
