6b16114a93da613d0785e401d43c61047fd63b69
ubuntu <aymenbelhadjkacem@gmail.com>
2021-07-15 04:40:18 -0700

    multiply

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,4 +3,4 @@ def multiply_by_2(a_dictionary):
     new_dictionary = {}
     for i, v in a_dictionary.items():
         new_dictionary[i] = v * 2
-return (new_dictionary)
+    return (new_dictionary)
