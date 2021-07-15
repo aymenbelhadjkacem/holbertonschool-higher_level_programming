a0b4399e2f0445899a4b1dbda1c9d37171bb6aff
ubuntu <aymenbelhadjkacem@gmail.com>
2021-07-15 04:57:22 -0700

    weigth_average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,4 +4,4 @@ def weight_average(my_list=[]):
         return (0)
     mul = list(map(lambda x: x[0] * x[1], my_list))
     add = list(map(lambda x: x[1], my_list))
-return sum(mul) / sum(add)
+    return sum(mul) / sum(add)
