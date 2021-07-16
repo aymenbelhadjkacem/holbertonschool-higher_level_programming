2424de00abe57905ed4b0f67328a52e1910620ce
ubuntu <aymenbelhadjkacem@gmail.com>
2021-07-16 06:39:33 -0700

    fix

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
+"""
+    Text Indention module
+    """
+
+
 def text_indentation(text):
-    if type(text) != str:
+    """
+    print text
+    2 new lines after each of these characters: ., ? and :
+    Args:
+        text (str): text
+    Raise
+        TypeError: when text is not str
+    """
+    if type(text) is not str:
         raise TypeError("text must be a string")
-    l = (".", "?", ":")
-    k = 0
-    x = [x for x in text.split(" ") if x.strip()]
-    nbwords = len(x)
-    for i in range(nbwords):
-        wordlen = len(x[i])
-        for c in range(wordlen):
-            char = x[i][c]
-            print(char, end="")
-            if char in l:
-                k = 1
-                print("\n" * 2, end="")
-        if i == nbwords - 2:
-            if x[i + 1] in l:
-                k = 1
-        if k == 0 and i != nbwords - 1:
-            print(" ", end="")
-        k = 0
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/5-text_indentation.txt")
+    a = 0
+    while a < len(text):
+        if text[a] in [':', '.', '?']:
+            print(text[a])
+            print()
+            a += 1
+        else:
+            print(text[a], end='')
+        a += 1
