e764e7886bfa8d58d24da57ec2946310e039db63
ubuntu <aymenbelhadjkacem@gmail.com>
2021-07-16 06:29:29 -0700

    fix

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+"""
+module that print Names
+    """
+
+
 def say_my_name(first_name, last_name=""):
-    if type(first_name) != str or len(first_name) is 0:
-        raise TypeError("first_name must be a string")
-    if type(last_name) != str:
-            raise TypeError("last_name must be a string")
-    print("My name is", first_name, last_name)
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/3-say_my_name.txt")
+    """
+        print name and last name
+        Args:
+            first_name (str): The first name
+            last_name (str): The last name
+        Raise:
+            TypeError: if first or last name is not string
+        Return: print My name is <first name> <last name>
+        """
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+    if type(last_name) is not str:
+        raise TypeError('last_name must be a string')
+    print("My name is {} {}".format(first_name, last_name))
