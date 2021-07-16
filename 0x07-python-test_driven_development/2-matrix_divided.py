10ab68038021a70abc19291f04c0c9a6eecc0169
ubuntu <aymenbelhadjkacem@gmail.com>
2021-07-16 06:05:11 -0700

    fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
-    types = (int, float)
-    errorMsg = "matrix must be a matrix (list of lists) of integers/floats"
-    if type(matrix) is not list:
-        raise TypeError(errorMsg)
-    if (isinstance(div, types) is False):
+    errorMessage = "matrix must be a matrix (list of lists) of integers/floats"
+    if not matrix:
+        raise TypeError(errorMessage)
+    if not isinstance(matrix, list):
+        raise TypeError(errorMessage)
+    for lists in matrix:
+        if not isinstance(lists, list):
+            raise TypeError(errorMessage)
+        for item in lists:
+            if not isinstance(item, int) and not isinstance(item, float):
+                raise TypeError(errorMessage)
+    for lists in matrix:
+        if len(lists) == 0:
+            raise TypeError(errorMessage)
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
-    c = list(map(lambda x:
-             list(map(lambda i: isinstance(i, types), x)), matrix))
-    for i in c:
-        if False in i:
-            raise TypeError(errorMsg)
-    matrixlen = list(map(lambda x: len(x), matrix))
-    if (len(set(matrixlen)) != 1):
+    if not all(len(lists) == len(matrix[0]) for lists in matrix):
         raise TypeError("Each row of the matrix must have the same size")
-    if div is 0:
+    if div == 0:
         raise ZeroDivisionError("division by zero")
-    new_matrix = list(map(lambda x: list(map(lambda i:
-                      round(i / div, 2), x)), matrix))
-    return (new_matrix)
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/2-matrix_divided.txt")
+return [[round(item / div, 2) for item in lists] for lists in matrix]
