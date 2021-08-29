d294942b70e755d53d76ae9a3e89c6c68fb97f0a
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2021-08-29 15:00:22 +0100

    r

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
-import { argv } from 'process';
 if(argv.length==0)
+{
 console.log('No argument');
+}
 else if (argv.length==1)
+{
 console.log('Argument found');
+}
 else
+{
 console.log('Arguments found');
+}
