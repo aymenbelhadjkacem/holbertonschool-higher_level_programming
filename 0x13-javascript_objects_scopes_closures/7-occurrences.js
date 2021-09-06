1c1ca65e206ac67c604243492302e137794585b7
aymenbelhadjkacem <aymenbelhadjkacem@gmail.com>
2021-09-06 16:33:19 +0100

    fixed

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-  let occurrences = 0;
-  for (let i = 0; i <= list.length; i++) {
-    if (list[i] === searchElement) {
-      occurrences++;
-    }
-  }
-  return occurrences;
+  return list.reduce((count, item) =>
+    (item === searchElement ? count + 1 : count), 0
+  );
 };
---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,9 @@
+
+#!/usr/bin/node
+
+let num = 0;
+
+exports.logMe = function (item) {
+  console.log(num + ': ' + item);
+  num++;
+};
